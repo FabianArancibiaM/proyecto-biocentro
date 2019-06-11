9f5ddb72ffeb02a9ef5ebb72c2034b205c29a4ac
fabian arancibia mejias <fabian.arancibia93@hotmail.com>
2019-06-10 23:04:04 -0400

    se sube clase faltante

---
+++ ProyectoIntegracionBiocentro/AplicacionWeb/RecibirPago.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RecibirPago.aspx.cs" Inherits="AplicacionWeb.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="card" style="left:10%;right:10%;width: 80%;position: relative;top:50px">
+            <h1 class="display-4" style="text-align:center" >Recibir Pago</h1>
+            <div class="card-header">
+                <h3 >Ingrese Rut Paciente:</h3>
+                <asp:TextBox ID="txtBuscarPaciente" runat="server" ></asp:TextBox>
+                <asp:Button ID="btnBuscarPaciente" runat="server" OnClick="btnBuscarPaciente_Click" Text="Buscar" CssClass="btn btn-primary" />
+            </div >
+            <hr style="color:black;margin-top: 3%; border-top: 4px solid"/>
+            <div class="modal-body">
+                <h3 ">Nombre:</h3>
+                <asp:TextBox ID="txtNombrePaciente" runat="server" Enabled="False"></asp:TextBox>
+                <h3 >Rut:</h3>
+                <asp:TextBox ID="txtRutPaciente1" runat="server" Enabled="False"></asp:TextBox>
+            </div>
+        </div>
+        <div style="left:10%;right:10%;width: 80%;position: relative;top:50px">
+             <asp:GridView ID="tablaResumen" runat="server" AutoGenerateColumns="false" CssClass="table table-striped">
+            <Columns>
+                <asp:TemplateField HeaderText="TERAPEUTA" >
+                    <ItemTemplate>
+                        <asp:Label ID="lblTerapeuta" runat="server" Enabled="false" Text='<%#Eval("Terapeuta.Nombre")+ " " + Eval("Terapeuta.ApellidoPaterno")+ " " + Eval("Terapeuta.ApellidoMaterno")%>'/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="EspecialidadClinica.Nombre" HeaderText="ESPECIALIDAD" />
+                <asp:BoundField DataField="Fecha" HeaderText="FECHA" />
+                <asp:TemplateField HeaderText="HORA" >
+                    <ItemTemplate>
+                        <asp:Label ID="lblHora" runat="server" Enabled="false" Text='<%#Eval("IdBloque.HoraInicio")+ " - " + Eval("IdBloque.HoraFin") + " Hrs" %>'/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="VALOR" >
+                    <ItemTemplate>
+                        <asp:Label ID="lblValor" runat="server" Enabled="false" Text='<%# "$ "+Eval("EspecialidadClinica.Precio")%>'/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Pagar" >
+                    <ItemTemplate>
+                        <asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="true" OnCheckedChanged="sumarHorasSeleccionadas" Enabled="true"/>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <emptydatatemplate>
+              <asp:image id="NoDataImage" runat="server" HeaderText="Pagar"/>                       
+                Sin Datos Para Mostrar.
+            </emptydatatemplate> 
+        </asp:GridView>
+        </div>
+        <div style="left:10%;right:10%;width: 80%;position: relative;top:50px">
+            <div >
+                <h3>Total:</h3>
+                <asp:TextBox ID="txtTotal" runat="server" />
+                <asp:Button ID="Button1" runat="server" OnClick="guardarPago" Text="Registrar Pago" CssClass="btn btn-primary" />
+            </div>
+            <asp:DropDownList id="ColorList"
+                AutoPostBack="True"
+                runat="server" CssClass="btn-group btn btn-secondary dropdown-toggle">
+                <asp:ListItem Selected="True" Value="White"> White </asp:ListItem>
+                <asp:ListItem Value="Silver"> Silver </asp:ListItem>
+                <asp:ListItem Value="DarkGray"> Dark Gray </asp:ListItem>
+                <asp:ListItem Value="Khaki"> Khaki </asp:ListItem>
+                <asp:ListItem Value="DarkKhaki"> Dark Khaki </asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ ProyectoIntegracionBiocentro/AplicacionWeb/RecibirPago.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AplicacionWeb
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnBuscarPaciente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.txtBuscarPaciente.Text.Length == 0)
+                {
+                    ShowMessage("Error: Ingrese un rut");
+                }
+                ServiceCliente.WebServiceClienteSoapClient soapClient = new ServiceCliente.WebServiceClienteSoapClient();
+                this.tablaResumen.DataSource = null;
+                this.tablaResumen.DataBind();
+                List<ServiceCliente.HoraAtencion> listReserva = new List<ServiceCliente.HoraAtencion>();
+                ServiceCliente.HoraAtencion[] listaSoap = soapClient.horasPorRutPacienteMasVentaService(this.txtBuscarPaciente.Text);
+                if (listaSoap != null)
+                {
+                    for (int i = 0; i < listaSoap.Length; i++)
+                    {
+                        listReserva.Add(listaSoap[i]);
+                    }
+                }
+                this.txtRutPaciente1.Text = listReserva.First().Paciente.Rut;
+                this.txtNombrePaciente.Text = listReserva.First().Paciente.Nombre + " " + listReserva.First().Paciente.ApellidoPaterno + " " + listReserva.First().Paciente.ApellidoMaterno;
+                this.tablaResumen.DataSource = listReserva;
+                this.tablaResumen.DataBind();
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Se produjo un error: " + ex.Message);
+            }
+        }
+        public void ShowMessage(string message)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("<script type = 'text/javascript'> ");
+            sb.Append("window.onload=function(){");
+            sb.Append("alert('");
+            sb.Append(message);
+            sb.Append("')};");
+            sb.Append("</script>");
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", sb.ToString());
+        }
+        protected void sumarHorasSeleccionadas(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsPostBack)
+                {
+                    int total = 0;
+                    for (int i = 0; i < this.tablaResumen.Rows.Count; i++)
+                    {
+                        GridViewRow row = this.tablaResumen.Rows[i];
+                        bool isChecked2 = ((CheckBox)row.FindControl("CheckBox1")).Checked;
+                        if (isChecked2)
+                        {
+                            int valor = Convert.ToInt32(((Label)row.FindControl("lblValor")).Text.Replace("$ ", ""));
+                            total = total + valor;
+                        }
+                    }
+                    this.txtTotal.Text = total.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Se produjo un error: " + ex.Message);
+            }
+        }
+
+        protected void guardarPago(object sender, EventArgs e)
+        {
+            try
+            {
+                List<ServiceCliente.HoraAtencion> listReserva = new List<ServiceCliente.HoraAtencion>();
+                for (int i = 0; i < this.tablaResumen.Rows.Count; i++)
+                {
+                    GridViewRow row = this.tablaResumen.Rows[i];
+                    bool isChecked2 = ((CheckBox)row.FindControl("CheckBox1")).Checked;
+                    if (isChecked2)
+                    {
+                        listReserva.Add((ServiceCliente.HoraAtencion)this.tablaResumen.Rows[i].DataItem);
+                    }
+                }
+                if (listReserva.Count<=0)
+                {
+                    ShowMessage("Error: Debe seleccionar una hora a pagar.");
+                    return;
+                }
+                int total = 0;
+                ServiceCliente.ArrayOfInt listaIdHora = new ServiceCliente.ArrayOfInt();
+                foreach (ServiceCliente.HoraAtencion h in listReserva)
+                {
+                    total = total + h.EspecialidadClinica.Precio;
+                    listaIdHora.Add(h.IdHora);
+                }
+                ServiceCliente.Venta venta = new ServiceCliente.Venta();
+                venta.FechaPago = DateTime.Now;
+                //venta.MedioPago = (ServiceCliente.MedioPago)this.comboMedioPago.SelectedItem;
+                venta.Monto = total;
+                ServiceCliente.EstadoVenta esVenta = new ServiceCliente.EstadoVenta();
+                esVenta.IdEstadoVenta = 2;
+                venta.EstadoVenta = esVenta;
+                ServiceCliente.WebServiceClienteSoapClient soapClient = new ServiceCliente.WebServiceClienteSoapClient();
+                ServiceCliente.StatusResponce responce = soapClient.guardarVentaRealizadaService(venta, listaIdHora);
+                if (responce.Estado.Equals("error"))
+                {
+                    ShowMessage("Se produjo un error: " + responce.Mensaje);
+                    return;
+                }
+                ShowMessage(responce.Mensaje);
+                return;
+            }
+            catch (Exception ex)
+            {
+                ShowMessage("Se produjo un error: " + ex.Message);
+            }
+        }
+
+    }
+}
